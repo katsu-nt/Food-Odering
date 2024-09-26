bcffd075bcee07ead4c2ae4e73f760f2f3eb81f4
Admin <benbilldev@gmail.com>
2024-09-26 22:30:03 +0700

    FE - Component - MobileNav: Create navigation for mobile's screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
     return (
         <div className="border-b-2 border-b-orange-500 py-6">
-            <div className="container mx-auto flex justify-between items-center"></div>
+            <div className="container mx-auto flex justify-between items-center">
             <Link className="text-3xl font-bold tracking-tight text-orange-500" to="/">
-            MearnEats.com</Link>
+                MearnEats.com</Link>
+            <div className="md:hidden">
+                <MobileNav />
+            </div>
+            </div>
+            
         </div>
     )
 }
---
+++ src/components/MainNav.tsx
@@ -0,0 +1,5 @@
+const MainNav = () => {
+  
+}
+
+export default MainNav;
