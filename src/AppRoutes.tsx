46fdadc028b5658ded77f6ac2e7f86fad10c589b
Admin <benbilldev@gmail.com>
2024-10-01 23:40:55 +0700

    FE - Auth: Update auth

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,13 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./layouts/layout"
 import HomePage from "./pages/HomePage"
+import AuthCallbackPage from "./pages/AuthCallbackPage"
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout><HomePage/></Layout>}></Route>
+            <Route path="/auth-callback" element={<AuthCallbackPage/>}></Route>
             <Route path="/user-profile" element={<span>User</span>}></Route>
             <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
         </Routes>
     )
 }
 export default AppRoutes
+
---
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,10 @@
+import { z } from "zod"
+const formSchema = z.object({
+    email: z.string().optional(),
+    name: z.string().min(1, "name is required"),
+    addressLine1: z.string().min(1, "Address Line 1 is required"),
+    city: z.string().min(1, "city is required"),
+    country: z.string().min(1, "Country is required"),
+})
+
+type UserFormData = z.infer<typeof formSchema>
