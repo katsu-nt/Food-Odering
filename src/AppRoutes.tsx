84ff44235e7fe005573ec703c725d6e2ae9cecea
Admin <benbilldev@gmail.com>
2024-10-08 11:42:26 +0700

    FE - Component: Create SearchBar

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,11 +5,13 @@ import AuthCallbackPage from "./pages/AuthCallbackPage"
 import UserProfilePage from "./pages/UserProfilePage"
 import ProtectedRoute from "./auth/ProtectedRoute"
 import ManageRestaurantPage from "./pages/ManageRestaurantPage"
+import SearchPage from "./pages/SearchPage"
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout showHero={true}><HomePage /></Layout>}></Route>
             <Route path="/auth-callback" element={<AuthCallbackPage />}></Route>
+            <Route path="/search/:city" element={<Layout showHero={false}><SearchPage/></Layout>}></Route>
             <Route element={<ProtectedRoute />}>
                 <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>}></Route>
                 <Route path="/manage-restaurant" element={<Layout><ManageRestaurantPage /></Layout>}></Route>
